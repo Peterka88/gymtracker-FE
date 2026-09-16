faeba05b6a1f33af3cc7692ab44d32ef03d9b529
Martin Petrina <martin.petrina11@gmail.com>
2026-09-16 23:47:38 +0200

    env name fix

--- src/pages/workouts/LocationPickerPage.tsx
+++ src/pages/workouts/LocationPickerPage.tsx
@@ -44,7 +44,7 @@ function LocationPickerPage() {
     const { showError, showSuccess } = useToast()
 
     const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: import.meta.env.GOOGLE_MAPS_API_KEY,
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
         libraries: mapLibraries,
     })
 
